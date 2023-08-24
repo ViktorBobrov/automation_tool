cf33e7711049d4d77f91f9e40e0bb8c2a17be229
ViktorBobrov <bobrov.voktor.93@gmail.com>
2023-08-24 14:29:45 +0700

    fix:modal

---
+++ app.js
@@ -0,0 +1,26 @@
+let myForm = ` <div class="form">
+<form class="form_block">
+  <label class="form_head">SEND US MESSAGE</label>
+  <label for="" class="labelName"> Full name</label>
+  <input type="text" placeholder="Name" class="inputName" />
+  <label for="" class="emailName">Email</label>
+  <input type="email" placeholder="Name" class="inputEmail" />
+  <label for="" class="messageName">Message</label>
+  <textarea
+	 name=""
+	 id=""
+	 cols="30"
+	 rows="5"
+	 class="inputTextarea"
+	 placeholder="Your Message"
+  ></textarea>
+  <button class="formButton">sumbit</button>
+</form>
+</div>
+<div class="overlay"></div>
+`;
+
+
+
+
+
